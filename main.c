4d299b3a4d73725498cbb57fb0cc385ac7f87f84
SimonBerens <sberensnyc@gmail.com>
2018-11-19 22:03:01 -0500

    Exec input commands

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "parse.h"
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    printf("Welcome to CShell\n");
+    while (1) {
+        printf("> ");
+        char command[1000];
+        scanf(" %[^\n]", command);
+        if (!fork()) {
+            char ** args = parse_args(command);
+            execvp(args[0], args);
+            exit(0); // todo: strerror
+        }
+        if (!strcmp(command, "exit")) return 0;
+    }
 }
